e78aaa4fdcead2cea4c7d9f178b12fb91ae40e60
Peter Williams <retex.williams@gmail.com>
2021-08-15 11:35:23 +0100

    redone main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const fetchUsers = async () =>
-    await (await fetch('.netflify/functions/getusers')).json();
+    await (await fetch('/.netlify/functions/getusers')).json();
 
 fetchUsers().then(data => {
     userList = document.querySelector('#users');
@@ -13,5 +13,5 @@ fetchUsers().then(data => {
         link.target = '_blank';
         li.appendChild(link);
         userList.appendChild(li);
-    })
-})
+    });
+});
